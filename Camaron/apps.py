61bb1ee90c4f3b0e297b8ed8ff6452811f670b46
carfmeza <carfmeza@espol.edu.ec>
2021-04-25 10:21:23 -0500

    Re-estructuración

---
+++ Camaron/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CamaronConfig(AppConfig):
+    name = 'Camaron'
--- camaron/views.py
+++ camaron/views.py
@@ -8,4 +8,3 @@ def home(request):
         'message': message,
     }
     return render(request, template, context)
-
