7007be42f6999bca5df589592c7b6571b08e827c
carfmeza <carfmeza@espol.edu.ec>
2021-04-25 10:26:23 -0500

    Re-estructuración

---
+++ Camaron/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+def home(request):
+    query = request.GET.get('name')
+    message = "El nombre escrito es: {}".format(query)
+    template = "home.html"
+    context = {
+        'message': message,
+    }
+    return render(request, template, context)
