6b2de8e5662992649c09413a69525a16a372af04
carfmeza <carfmeza@espol.edu.ec>
2021-04-27 23:08:58 -0500

    Más grande el logo

--- Camaron/static/rsc/js/top.js
+++ Camaron/static/rsc/js/top.js
@@ -4,14 +4,15 @@ function scrollFunction() {
   const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
   if (document.body.scrollTop > vw*0.1 || document.documentElement.scrollTop > vw*0.1) {
     // Logo
-    document.getElementById("logo").style.maxHeight = "7.5vw";
-    document.getElementById("logo").style.left = "46.25vw";
+    document.getElementById("logo").style.maxHeight = "7vw";
+    document.getElementById("logo").style.left = "46.5vw";
+    document.getElementById("logo").style.top = "1.75vw";
     // Nav
     document.getElementsByClassName("navbar")[0].style.maxHeight = "8vw";
   } else {
     // Logo
-    document.getElementById("logo").style.maxHeight = "10.5vw";
-    document.getElementById("logo").style.left = "44.75vw";
+    document.getElementById("logo").style.maxHeight = "14vw";
+    document.getElementById("logo").style.left = "43vw";
     // Nav
     document.getElementsByClassName("navbar")[0].style.maxHeight = "10vw";
   }
