c95b3d5528a057ccfc09559a7a23ece8e5bffdc2
carfmeza <carfmeza@espol.edu.ec>
2021-04-28 22:56:26 -0500

    Re-estructuración y posición del carrousel
    TODO: Espacio en blanco entre fotos del carrousel

--- Camaron/static/rsc/js/top.js
+++ Camaron/static/rsc/js/top.js
@@ -5,9 +5,8 @@ function scrollFunction() {
   const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
   if (document.body.scrollTop > vw*0.1 || document.documentElement.scrollTop > vw*0.1) {
     // Logo
-    document.getElementById("logo").style.maxHeight = "7vw";
-    document.getElementById("logo").style.left = "46.5vw";
-    document.getElementById("logo").style.top = "1.75vw";
+    document.getElementById("logo").style.maxHeight = "6.25vw";
+    document.getElementById("logo").style.left = "46.875vw";
     // Nav
     document.getElementsByClassName("navbar")[0].style.maxHeight = "8vw";
   } else {
