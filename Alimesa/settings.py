aaf18f7c5d61637493e28a4109bbf0b030b12d2e
carfmeza <carfmeza@espol.edu.ec>
2021-05-01 10:53:18 -0500

    Prep for Heroku

--- Alimesa/settings.py
+++ Alimesa/settings.py
@@ -123,3 +123,11 @@ STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+########################
+
+import dj_database_url
+prod_db  =  dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
+
+########################
