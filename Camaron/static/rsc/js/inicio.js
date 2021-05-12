84bf309f3daa3fbab53d2dca75f43097d920bd84
carfmeza <carfmeza@espol.edu.ec>
2021-05-12 01:59:32 -0500

    Inicio y Prod, primeros pasos responsive y transición a VH

--- Camaron/static/rsc/js/inicio.js
+++ Camaron/static/rsc/js/inicio.js
@@ -2,29 +2,35 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
+  const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
 
   // Cambiar tamaño
   if (document.body.scrollTop > vw*0.1 || document.documentElement.scrollTop > vw*0.1) {
     // Logo
-    document.getElementById("logo").style.maxHeight = "6.25vw";
-    document.getElementById("logo").style.left = "46.875vw";
+    document.getElementById("logo").style.height = "8.25vh";
+    document.getElementById("logo").style.left = "calc(50vw - (8.25vh/2))";
+    document.getElementById("logo").style.top = "2.5vh";
     // Nav
-    document.getElementsByClassName("navbar")[0].style.maxHeight = "8vw";
+    document.getElementsByClassName("navbar")[0].style.maxHeight = "10vh";
+    document.getElementsByClassName("navbar")[0].style.height = "10vh";
   } else {
     // Logo
-    document.getElementById("logo").style.maxHeight = "14vw";
-    document.getElementById("logo").style.left = "43vw";
+    document.getElementById("logo").style.height = "calc(var(--LinksSeccionesHeight) + 5.5vh)";
+    document.getElementById("logo").style.left = "calc(50vw - ((var(--LinksSeccionesHeight) + 5.5vh)/2))";
+    document.getElementById("logo").style.top = "3.75vh";
     // Nav
-    document.getElementsByClassName("navbar")[0].style.maxHeight = "10vw";
+    document.getElementsByClassName("navbar")[0].style.maxHeight = "15vh";
+    document.getElementsByClassName("navbar")[0].style.height = "15vh";
   }
 
   // Cambiar color
   // MaxHeight del carouselExampleCaptions + 0.015
-  if (document.body.scrollTop > vw*0.4375 || document.documentElement.scrollTop > vw*0.4375) {
+  if (document.body.scrollTop > vh*0.9 || document.documentElement.scrollTop > vh*0.9) {
     // Logo
     document.getElementById("logo").src = "/static/rsc/img/Recurso 4.png";
-    document.getElementById("logo").style.maxHeight = "5.5vw";
-    document.getElementById("logo").style.left = "47.25vw";
+    document.getElementById("logo").style.height = "6vh";
+    document.getElementById("logo").style.left = "calc(50vw - (6vh/2))";
+    document.getElementById("logo").style.top = "3vh";
     // Nav
     document.getElementsByClassName("navbar")[0].style.backgroundColor = "var(--azul-nav)";
     //Titulos
--- Camaron/static/rsc/js/prods.js
+++ Camaron/static/rsc/js/prods.js
@@ -1,26 +1,35 @@
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
   const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
 
   // Cambiar tamaño
-  if (document.body.scrollTop > vw*0.01 || document.documentElement.scrollTop > vw*0.01) {
+  if (document.body.scrollTop > vw * 0.01 || document.documentElement.scrollTop > vw * 0.01) {
+    // Logo
+    document.getElementById("logo").style.height = "8.25vh";
+    document.getElementById("logo").style.left = "calc(50vw - (8.25vh/2))";
+    document.getElementById("logo").style.top = "2.5vh";
     // Logo
     document.getElementById("logo").src = "/static/rsc/img/Recurso 4.png";
-    document.getElementById("logo").style.maxHeight = "6.25vw";
-    document.getElementById("logo").style.left = "46.875vw";
+    document.getElementById("logo").style.height = "6vh";
+    document.getElementById("logo").style.left = "calc(50vw - (6vh/2))";
+    document.getElementById("logo").style.top = "3vh";
     // Nav
-    document.getElementsByClassName("navbar")[0].style.maxHeight = "8vw";
+    document.getElementsByClassName("navbar")[0].style.maxHeight = "10vh";
+    document.getElementsByClassName("navbar")[0].style.height = "10vh";
   } else {
     // Logo
+    document.getElementById("logo").style.height = "calc(var(--LinksSeccionesHeight) + 5.5vh)";
+    document.getElementById("logo").style.left = "calc(50vw - ((var(--LinksSeccionesHeight) + 5.5vh)/2))";
+    document.getElementById("logo").style.top = "3.75vh";
+    // Logo
     document.getElementById("logo").src = "/static/rsc/img/logoAzultri.png";
-    document.getElementById("logo").style.maxHeight = "14vw";
-    document.getElementById("logo").style.left = "43vw";
     // Nav
-    document.getElementsByClassName("navbar")[0].style.maxHeight = "10vw";
+    document.getElementsByClassName("navbar")[0].style.maxHeight = "15vh";
+    document.getElementsByClassName("navbar")[0].style.height = "15vh";
   }
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-    scrollFunction();
+document.addEventListener('DOMContentLoaded', function () {
+  scrollFunction();
 })
