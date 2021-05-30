2ba5a8ad9366830451b8adb9d184e2aa84299979
Oscar Avila <oscaravilaa9817@gmail.com>
2021-05-30 03:00:18 -0500

    nuevo nav bar

--- Camaron/static/rsc/js/inicio.js
+++ Camaron/static/rsc/js/inicio.js
@@ -1,68 +1,51 @@
-window.onscroll = function() {scrollFunction()};
+
+window.onscroll = function() {
+  scrollFunction()
+};
 
 function scrollFunction() {
   const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
   const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
-
-  // Cambiar tamaño
-  if (document.body.scrollTop > vw*0.1 || document.documentElement.scrollTop > vw*0.1) {
-    // Logo
-    document.getElementById("logo").style.height = "8.25vh";
-    document.getElementById("logo").style.left = "calc(50vw - (8.25vh/2))";
-    document.getElementById("logo").style.top = "2.5vh";
-    // Nav
-    document.getElementsByClassName("navbar")[0].style.maxHeight = "10vh";
-    document.getElementsByClassName("navbar")[0].style.height = "10vh";
-  } else {
-    // Logo
-    document.getElementById("logo").style.height = "calc(var(--LinksSeccionesHeight) + 5.5vh)";
-    document.getElementById("logo").style.left = "calc(50vw - ((var(--LinksSeccionesHeight) + 5.5vh)/2))";
-    document.getElementById("logo").style.top = "3.75vh";
-    // Nav
-    document.getElementsByClassName("navbar")[0].style.maxHeight = "15vh";
-    document.getElementsByClassName("navbar")[0].style.height = "15vh";
-  }
-
   // Cambiar color
   // MaxHeight del carouselExampleCaptions + 0.015
   if (document.body.scrollTop > vh*0.9 || document.documentElement.scrollTop > vh*0.9) {
     // Logo
-    document.getElementById("logo").src = "/static/rsc/img/Recurso 4.png";
-    document.getElementById("logo").style.height = "6vh";
-    document.getElementById("logo").style.left = "calc(50vw - (6vh/2))";
-    document.getElementById("logo").style.top = "3vh";
+    document.getElementById("logo").src = "/static/rsc/img/LogoCamaronBlanco.png";
+    document.getElementById("logo").style.height = "16vh";
+    document.getElementById("logo").style.width = "8vw";
     // Nav
-    document.getElementsByClassName("navbar")[0].style.backgroundColor = "var(--azul-nav)";
+    document.getElementsByClassName("nav-container")[0].style.background = "linear-gradient(180deg, rgba(0, 99, 181, 1) 0%, rgba(0, 79, 157, 1) 50%, rgba(0, 55, 130, 1) 100%)";
+    document.getElementsByClassName("contactosEidioma-nav")[0].style.background = "rgb(255, 255, 255)";
     document.getElementById("tlf").src = "/static/rsc/img/tlfAzul.png";
     document.getElementById("mail").src = "/static/rsc/img/mailAzul.png";
     //Titulos
-    Array.prototype.forEach.call(document.getElementsByClassName("TituloSeccion"), function(n) {
-      n.style.color = "var(--blanco-nav)";
+    Array.prototype.forEach.call(document.getElementsByClassName("opcionMenu"), function(n) {
+      n.style.color = "rgb(255, 255, 255)";
     });
-    // TODO: Cambiar las imagenes del teléfono y del sobre
     // DatosEIdioma
-    document.getElementsByClassName("DatosEIdioma")[0].style.backgroundColor = "var(--blanco-nav)";
     Array.prototype.forEach.call(document.getElementsByClassName("linksCEI"), function(n) {
-      n.style.color = "var(--azul-titulos)";
+      n.style.color = "#00509A";
     });
   } else {
-    // Logo
-    document.getElementById("logo").src = "/static/rsc/img/logoAzultri.png";
+    document.getElementById("logo").src = "/static/rsc/img/logoAzul.png";
+    document.getElementById("logo").style.height = "15vh";
+    document.getElementById("logo").style.width = "6vw";
     // Nav
-    document.getElementsByClassName("navbar")[0].style.backgroundColor = "var(--blanco-nav)";
+    document.getElementsByClassName("nav-container")[0].style.background = "rgb(255, 255, 255)";
     document.getElementById("tlf").src = "/static/rsc/img/phone.png";
     document.getElementById("mail").src = "/static/rsc/img/mail.png";
     //Titulos
-    Array.prototype.forEach.call(document.getElementsByClassName("TituloSeccion"), function(n) {
-      n.style.color = "var(--azul-nav)";
+    Array.prototype.forEach.call(document.getElementsByClassName("opcionMenu"), function(n) {
+      n.style.color = "rgb(0, 0, 0)";
     });
     // DatosEIdioma
-    document.getElementsByClassName("DatosEIdioma")[0].style.backgroundColor = "var(--azul-nav)";
+    document.getElementsByClassName("contactosEidioma-nav")[0].style.background = "linear-gradient(180deg, rgba(0, 99, 181, 1) 0%, rgba(0, 79, 157, 1) 50%, rgba(0, 55, 130, 1) 100%)";
     Array.prototype.forEach.call(document.getElementsByClassName("linksCEI"), function(n) {
-      n.style.color = "var(--blanco-nav)";
+      n.style.color = "rgb(255, 255, 255)";
     });
   }
 }
+
 
 document.addEventListener('DOMContentLoaded', function() {
   scrollFunction();
--- Camaron/static/rsc/js/prods.js
+++ Camaron/static/rsc/js/prods.js
@@ -1,37 +1,17 @@
-window.onscroll = function () { scrollFunction() };
-
-function scrollFunction() {
-  const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
-  document.getElementById("tlf").src = "/static/rsc/img/tlfAzul.png";
-  document.getElementById("mail").src = "/static/rsc/img/mailAzul.png";
-
-  // Cambiar tamaño
-  if (document.body.scrollTop > vw * 0.01 || document.documentElement.scrollTop > vw * 0.01) {
-    // Logo
-    document.getElementById("logo").style.height = "8.25vh";
-    document.getElementById("logo").style.left = "calc(50vw - (8.25vh/2))";
-    document.getElementById("logo").style.top = "2.5vh";
-    // Logo
-    document.getElementById("logo").src = "/static/rsc/img/Recurso 4.png";
-    document.getElementById("logo").style.height = "6vh";
-    document.getElementById("logo").style.left = "calc(50vw - (6vh/2))";
-    document.getElementById("logo").style.top = "3vh";
-    // Nav
-    document.getElementsByClassName("navbar")[0].style.maxHeight = "10vh";
-    document.getElementsByClassName("navbar")[0].style.height = "10vh";
-  } else {
-    // Logo
-    document.getElementById("logo").style.height = "calc(var(--LinksSeccionesHeight) + 5.5vh)";
-    document.getElementById("logo").style.left = "calc(50vw - ((var(--LinksSeccionesHeight) + 5.5vh)/2))";
-    document.getElementById("logo").style.top = "3.75vh";
-    // Logo
-    document.getElementById("logo").src = "/static/rsc/img/logoAzultri.png";
-    // Nav
-    document.getElementsByClassName("navbar")[0].style.maxHeight = "15vh";
-    document.getElementsByClassName("navbar")[0].style.height = "15vh";
-  }
-}
-
-document.addEventListener('DOMContentLoaded', function () {
-  scrollFunction();
-})
+// Logo
+document.getElementById("logo").src = "/static/rsc/img/LogoCamaronBlanco.png";
+document.getElementById("logo").style.height = "16vh";
+document.getElementById("logo").style.width = "8vw";
+// Nav
+document.getElementsByClassName("nav-container")[0].style.background = "linear-gradient(180deg, rgba(0, 99, 181, 1) 0%, rgba(0, 79, 157, 1) 50%, rgba(0, 55, 130, 1) 100%)";
+document.getElementsByClassName("contactosEidioma-nav")[0].style.background = "rgb(255, 255, 255)";
+document.getElementById("tlf").src = "/static/rsc/img/tlfAzul.png";
+document.getElementById("mail").src = "/static/rsc/img/mailAzul.png";
+//Titulos
+Array.prototype.forEach.call(document.getElementsByClassName("opcionMenu"), function(n) {
+  n.style.color = "rgb(255, 255, 255)";
+});
+// DatosEIdioma
+Array.prototype.forEach.call(document.getElementsByClassName("linksCEI"), function(n) {
+  n.style.color = "#00509A";
+});
