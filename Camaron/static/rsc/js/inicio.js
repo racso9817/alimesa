6425c4d004104440866a704eb674eaa9501b3d7a
carfmeza <carfmeza@espol.edu.ec>
2021-06-02 06:55:44 -0500

    Cambios al nav

--- Camaron/static/rsc/js/inicio.js
+++ Camaron/static/rsc/js/inicio.js
@@ -13,7 +13,7 @@ function scrollFunction() {
     document.getElementById("logo").src = "/static/rsc/img/LogoCamaronBlanco.png";
     document.getElementById("logo").style.height = "8vw";
     document.getElementById("logo").style.width = "7vw";
-    document.getElementsByClassName("logo-nav")[0].style.paddingTop = "2vw"; 
+    document.getElementsByClassName("logo-nav")[0].style.paddingTop = "1.5vw"; 
     // Nav
     document.getElementsByClassName("nav-container")[0].style.background = "linear-gradient(180deg, rgba(0, 99, 181, 1) 0%, rgba(0, 79, 157, 1) 50%, rgba(0, 55, 130, 1) 100%)";
     document.getElementsByClassName("contactosEidioma-nav")[0].style.background = "rgb(255, 255, 255)";
@@ -28,26 +28,26 @@ function scrollFunction() {
       n.style.color = "#00509A";
     });
     //Contacto E Idioma
-    document.getElementsByClassName("navbar")[0].style.marginTop = "-2.8vw";
+    // document.getElementsByClassName("navbar")[0].style.marginTop = "-2.8vw";
   } else {
     document.getElementById("logo").src = "/static/rsc/img/logoAzul.png";
     document.getElementById("logo").style.height = "6vw";
     document.getElementById("logo").style.width = "7.9vw";
-    document.getElementsByClassName("logo-nav")[0].style.paddingTop = "2.5vw";
+    document.getElementsByClassName("logo-nav")[0].style.paddingTop = "2vw";
     //Contacto E Idioma
-    document.getElementsByClassName("navbar")[0].style.marginTop = "-2.2vw";
+    // document.getElementsByClassName("navbar")[0].style.marginTop = "-2.2vw";
     // Nav
-    document.getElementsByClassName("nav-container")[0].style.background = "rgb(255, 255, 255)";
+    document.getElementsByClassName("nav-container")[0].style.background = "white";
     document.getElementById("tlf").src = "/static/rsc/img/phone.png";
     document.getElementById("mail").src = "/static/rsc/img/mail.png";
     //Titulos
     Array.prototype.forEach.call(document.getElementsByClassName("opcionMenu"), function(n) {
-      n.style.color = "rgb(0, 0, 0)";
+      n.style.color = "var(--azul-nav)";
     });
     // DatosEIdioma
     document.getElementsByClassName("contactosEidioma-nav")[0].style.background = "linear-gradient(180deg, rgba(0, 99, 181, 1) 0%, rgba(0, 79, 157, 1) 50%, rgba(0, 55, 130, 1) 100%)";
     Array.prototype.forEach.call(document.getElementsByClassName("linksCEI"), function(n) {
-      n.style.color = "rgb(255, 255, 255)";
+      n.style.color = "white";
     });
   }
 }
