037bdfac5abcd016e9c7ea74469e13625a334511
Oscar Avila <oscaravilaa9817@gmail.com>
2021-06-06 02:41:00 -0500

    prueba de modal lightbox images

--- Camaron/static/rsc/js/modals.js
+++ Camaron/static/rsc/js/modals.js
@@ -1,6 +1,33 @@
 // Get the modal
 var modal = document.getElementById("myModal");
 
+// function openModal() {
+//   modal.style.display = 'block';
+// }
+
+// var slideIndex = 1;
+// showSlides(slideIndex);
+
+// function plusSlides(n) {
+//   showSlides(slideIndex += n);
+// }
+
+// function currentSlide(n) {
+//   showSlides(slideIndex = n);
+// }
+
+// function showSlides(n) {
+//   var i;
+//   var slides = document.getElementsByClassName("imgMarca");
+//   if (n > slides.length) {slideIndex = 1}
+//   if (n < 1) {slideIndex = slides.length}
+//   for (i = 0; i < slides.length; i++) {
+//       slides[i].style.display = "none";
+//   }
+//   slides[slideIndex-1].style.display = "block";
+// }
+
+
 // Get the image and insert it inside the modal - use its "alt" text as a caption
 var img = document.getElementsByClassName("imgMarca");
 var modalImg = document.getElementById("img1");
@@ -14,9 +41,9 @@ for (var i = 0; i < img.length; i++) {
 }
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("modal")[0];
+ var span = document.getElementsByClassName("modal")[0];
 
 
-span.onclick = function () {
-  modal.style.display = "none";
-}
+ span.onclick = function () {
+   modal.style.display = "none";
+ }
