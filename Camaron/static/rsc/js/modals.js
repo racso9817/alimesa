d9021371c3202fbc9e8c354d365b76175ad992a0
Oscar Avila <oscaravilaa9817@gmail.com>
2021-06-06 02:11:31 -0500

    arreglo del archivo modals.js

--- Camaron/static/rsc/js/modals.js
+++ Camaron/static/rsc/js/modals.js
@@ -1,54 +1,21 @@
 // Get the modal
 var modal = document.getElementById("myModal");
 
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById("myImg1");
-var img2 = document.getElementById("myImg2");
-var img3 = document.getElementById("myImg3");
-var img4 = document.getElementById("myImg4");
-var img5 = document.getElementById("myImg5");
-var img6 = document.getElementById("myImg6");
+// Get the image and insert it inside the modal 
+var img = document.getElementsByClassName("imgMarca");
 var modalImg = document.getElementById("img1");
-var captionText = document.getElementById("caption");
-img.onclick = function(){
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
+
+for (var i = 0; i < images.length; i++) {
+  img.onclick = function () {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+  }
 }
 
-img2.onclick = function(){
-    modal.style.display = "block";
-    modalImg.src = this.src;
-    captionText.innerHTML = this.alt;
-}
-
-img3.onclick = function(){
-    modal.style.display = "block";
-    modalImg.src = this.src;
-    captionText.innerHTML = this.alt;
-  }
-
-img4.onclick = function(){
-    modal.style.display = "block";
-    modalImg.src = this.src;
-    captionText.innerHTML = this.alt;
-  }
-
-  img5.onclick = function(){
-    modal.style.display = "block";
-    modalImg.src = this.src;
-    captionText.innerHTML = this.alt;
-  }
-
-  img6.onclick = function(){
-    modal.style.display = "block";
-    modalImg.src = this.src;
-    captionText.innerHTML = this.alt;
-  }
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("modal")[0];
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() { 
+
+span.onclick = function () {
   modal.style.display = "none";
 }
