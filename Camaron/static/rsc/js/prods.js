0c1f4ae4bdbdedc40613c59c936e5dee50bd1cfc
Oscar Avila <oscaravilaa9817@gmail.com>
2021-06-13 14:09:48 -0500

    eliminar cosas innecesarias

--- Camaron/static/rsc/js/prods.js
+++ Camaron/static/rsc/js/prods.js
@@ -16,31 +16,31 @@ Array.prototype.forEach.call(document.getElementsByClassName("linksCEI"), functi
   n.style.color = "#00509A";
 });
 
-//MEDIA QUERIES
-const x = window.matchMedia("(max-width: 961px)")
+// //MEDIA QUERIES
+// const x = window.matchMedia("(max-width: 961px)")
 
-  if (x.matches) { // If media query matches
-    // Logo
-    document.getElementById("logo").src = "/static/rsc/img/LogoCamaronBlanco.png";
-    document.getElementById("logo").style.height = "6vh";
-    document.getElementById("logo").style.width = "7.5vh";
-  }
-  else {
-    // Logo
-    document.getElementById("logo").src = "/static/rsc/img/LogoCamaronBlanco.png";
-    document.getElementById("logo").style.height = "8vw";
-    document.getElementById("logo").style.width = "7vw";
-    // Nav
-    document.getElementsByClassName("nav-container")[0].style.background = "linear-gradient(180deg, rgba(0, 99, 181, 1) 0%, rgba(0, 79, 157, 1) 50%, rgba(0, 55, 130, 1) 100%)";
-    document.getElementsByClassName("contactosEidioma-nav")[0].style.background = "rgb(255, 255, 255)";
-    document.getElementById("tlf").src = "/static/rsc/img/tlfAzul.png";
-    document.getElementById("mail").src = "/static/rsc/img/mailAzul.png";
-    //Titulos
-    Array.prototype.forEach.call(document.getElementsByClassName("opcionMenu"), function(n) {
-      n.style.color = "rgb(255, 255, 255)";
-    });
-    // DatosEIdioma
-    Array.prototype.forEach.call(document.getElementsByClassName("linksCEI"), function(n) {
-      n.style.color = "#00509A";
-    });
-  }
+//   if (x.matches) { // If media query matches
+//     // Logo
+//     document.getElementById("logo").src = "/static/rsc/img/LogoCamaronBlanco.png";
+//     document.getElementById("logo").style.height = "6vh";
+//     document.getElementById("logo").style.width = "7.5vh";
+//   }
+//   else {
+//     // Logo
+//     document.getElementById("logo").src = "/static/rsc/img/LogoCamaronBlanco.png";
+//     document.getElementById("logo").style.height = "8vw";
+//     document.getElementById("logo").style.width = "7vw";
+//     // Nav
+//     document.getElementsByClassName("nav-container")[0].style.background = "linear-gradient(180deg, rgba(0, 99, 181, 1) 0%, rgba(0, 79, 157, 1) 50%, rgba(0, 55, 130, 1) 100%)";
+//     document.getElementsByClassName("contactosEidioma-nav")[0].style.background = "rgb(255, 255, 255)";
+//     document.getElementById("tlf").src = "/static/rsc/img/tlfAzul.png";
+//     document.getElementById("mail").src = "/static/rsc/img/mailAzul.png";
+//     //Titulos
+//     Array.prototype.forEach.call(document.getElementsByClassName("opcionMenu"), function(n) {
+//       n.style.color = "rgb(255, 255, 255)";
+//     });
+//     // DatosEIdioma
+//     Array.prototype.forEach.call(document.getElementsByClassName("linksCEI"), function(n) {
+//       n.style.color = "#00509A";
+//     });
+//   }
